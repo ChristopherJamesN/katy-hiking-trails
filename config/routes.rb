23516ecd848cee7a9c5d1a96ad0a6763b9b0067e
ChristopherJamesN <nady.christopher@gmail.com>
2018-01-14 14:25:22 -0600

    Add api scope routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   get 'trails/favorite/:id', to: 'trails#favorite'
   get 'trails/remove/:id', to: 'trails#remove'
   get 'trails/favorites', to: 'trails#favorites'
   get '/trails/addicks', to: 'trails#addicks'
   get '/trails/barker', to: 'trails#barker'
   get '/trails/comment/:id', to: 'trail_users#update'
+
+  post '/user_token' => 'user_token#create'
+
+  scope '/api' do
+    post '/register' => 'users#create'
+    devise_for :users
+    mount Knock::Engine => '/knock'
+  end
 
   resources :charges
   resources :trails
