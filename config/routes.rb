491035222334372b4118f6a6107470a464b67363
ChristopherJamesN <nady.christopher@gmail.com>
2018-01-14 14:31:04 -0600

    Add user_token_controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   get 'trails/favorite/:id', to: 'trails#favorite'
   get 'trails/remove/:id', to: 'trails#remove'
   get 'trails/favorites', to: 'trails#favorites'
