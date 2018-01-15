023d64063c59666e9aa306500f7a2a0730b3f3ae
ChristopherJamesN <nady.christopher@gmail.com>
2018-01-15 07:14:15 -0600

    Remove has_secure_password from user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_secure_password
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  #has_secure_password
   has_many :trail_users
   has_many :trails, through: :trail_users
 end
