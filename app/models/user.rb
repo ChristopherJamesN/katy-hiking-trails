85bb4891d45ce0927763400a83071b9919cc31fc
ChristopherJamesN <nady.christopher@gmail.com>
2018-01-15 08:37:51 -0600

    Add an authenticate method to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable #:confirmable, :lockable, :timeoutable, :omniauthable
 
   #has_secure_password
+
+  def authenticate(unencrypted_password)
+    BCrypt::Password.new(password_digest).is_password?(unencrypted_password) && self
+  end
+
   has_many :trail_users
   has_many :trails, through: :trail_users
 end
